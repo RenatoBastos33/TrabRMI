f116dc2e192020de6cb9482b6d548d804c3143af
renatobastos33 <renato_bastos@id.uff.br>
2018-10-23 19:04:03 -0200

    Semaforo pronto?

--- sisdis/src/trabalhoServidor.java
+++ sisdis/src/trabalhoServidor.java
@@ -9,7 +9,7 @@ public class trabalhoServidor implements trabalho{
     private Arquivo[] listaArquivos;
     private trabalhoServidor(Arquivo[] listaArquivos, Semaphore[][] semaforos){
             this.listaArquivos = listaArquivos;
-            this.semaforos = semaforos
+            this.semaforos = semaforos;
     }
     public static void main(String[] args) {
         Arquivo file1 = new Arquivo("arq1.txt");
@@ -40,7 +40,11 @@ public class trabalhoServidor implements trabalho{
     @Override
     public boolean escreverRMI(String texto, int arq) {
         try {
+            semaforos[arq][1].acquire();
+            semaforos[arq][0].acquire(3);
             listaArquivos[arq].escrever(texto);
+            semaforos[arq][1].release();
+            semaforos[arq][0].release(3);
             return true;
         }
         catch(Exception e) {
@@ -51,6 +55,16 @@ public class trabalhoServidor implements trabalho{
 
     @Override
     public String lerRMI(int ini, int fim, int arq) {
+        try{
+            semaforos[arq][0].acquire();
+            listaArquivos[arq].ler(ini,fim);
+            semaforos[arq][0].release();
+        }catch(Exception e) {
+            System.err.println("Exceção: " + e.toString());
+            return "Deu erro :"+ e;
+        }
+
+
         return null;
     }
 }
